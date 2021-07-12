021529bf91b1305ba5ce0239e0253e31ae3163b4
ribbas <sab61r.a@gmail.com>
2021-07-12 13:31:36 -0400

    add imbalanced example tree, add method for listing all leaves, generate new objects when creating subtrees

--- app/compressedtree.py
+++ app/compressedtree.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 class CompressedNode:
     def __init__(
-        self, class_name, node_id=-1, links=None, module=None, name=None
+        self, class_name=None, node_id=-1, links=None, module=None, name=None
     ) -> None:
 
         self.class_name = class_name
@@ -16,7 +16,7 @@ class CompressedNode:
         self.name = name
 
     def __repr__(self) -> str:
-        return f"{self.name} ({self.node_id})"
+        return f"{self.name} ({id(self)})"
 
     def __eq__(self, other) -> bool:
         if isinstance(other, CompressedNode):
@@ -27,28 +27,63 @@ class CompressedNode:
 
 
 class CompressedTree:
-    def __init__(self, tree) -> None:
-        self.root = CompressedNode("Home")
+    def __init__(self, composition) -> None:
 
-        self.tree = tree
+        for k in composition:
+            if k["module"].class_name == "Home":
+                self.root = k["module"]
+        self.composition = composition
+
+        self.leaves = []
+        self.tree = {}
 
     def chain(self) -> None:
 
-        self.compressed = {e["module"]: e["elements"] for e in self.tree}
+        self.compressed = {e["module"]: e["elements"] for e in self.composition}
 
     def __get_children(self, node) -> list:
 
         for parent in self.compressed:
             if parent == node:
-                return self.compressed[parent]
+                return [
+                    CompressedNode(
+                        class_name=i.class_name,
+                        node_id=i.node_id,
+                        links=i.links,
+                        module=i.module,
+                        name=i.name,
+                    )
+                    for i in self.compressed[parent]
+                ]
 
         return []
 
-    def decompress(self, node) -> dict:
-        return {node: [self.decompress(n) for n in self.__get_children(node)]}
+    def __decompress(self, node) -> dict:
+        return {node: [self.__decompress(n) for n in self.__get_children(node)]}
+
+    def decompress(self) -> dict:
+        self.tree = self.__decompress(self.root)
+
+    def get_tree(self) -> dict:
+
+        return self.tree
+
+    def __get_leaves(self, subtree) -> None:
+        for key, value in subtree.items():
+            if type(value) is list:
+                if not value:
+                    self.leaves.append(key)
+
+                for i in value:
+                    self.__get_leaves(i)
+
+    def get_leaves(self) -> list:
+
+        self.__get_leaves(self.tree)
+        return self.leaves
 
 
-example_tree = [
+example_comp = [
     {
         "elements": [
             CompressedNode(
@@ -59,7 +94,7 @@ example_tree = [
                 name="eight#0",
             )
         ],
-        "module": CompressedNode("Home"),
+        "module": CompressedNode("Home", name="root"),
     },
     {
         "elements": [
@@ -118,12 +153,22 @@ example_tree = [
                 module="four",
                 name="two#1",
             ),
+            CompressedNode(
+                class_name="fulladder",
+                node_id=4,
+                links=[],
+                module="two",
+                name="fulladder#0",
+            ),
         ],
         "module": CompressedNode("eight"),
     },
 ]
 
 
-tree = CompressedTree(example_tree)
-tree.chain()
-pprint(tree.decompress(tree.root))
+tree_obj = CompressedTree(example_comp)
+tree_obj.chain()
+tree_obj.decompress()
+pprint(tree_obj.tree)
+
+# print(tree_obj.get_leaves())
